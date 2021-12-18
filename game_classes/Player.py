493c78b683c984a46877795716b44ff7ed3c6877
Alex-sys-sys <94735806+Alex-sys-sys@users.noreply.github.com>
2021-12-18 12:55:39 +0300

    class player: get_cords function

--- game_classes/Player.py
+++ game_classes/Player.py
@@ -5,6 +5,8 @@ class Player(pp.sprite.Sprite):
     def __init__(self, screen):
         pp.sprite.Sprite.__init__(self)
         self.rect = pp.Rect(0, 0, 40, 40)
+        self.x = 0
+        self.y = 0
         self.screen = screen
         self.hp = 100
 
@@ -12,10 +14,13 @@ class Player(pp.sprite.Sprite):
         pp.draw.rect(self.screen, (0, 0, 255), self.rect)
         key = pp.key.get_pressed()
         if key[pp.K_LEFT] and self.rect.x > 0:
-            self.rect.x -= 2
+            self.x -= 2
         if key[pp.K_RIGHT] and self.rect.x < 500:
-            self.rect.x += 2
+            self.x += 2
         if key[pp.K_UP] and self.rect.y > 0:
-            self.rect.y -= 2
+            self.y -= 2
         if key[pp.K_DOWN] and self.rect.y < 500:
-            self.rect.y += 2
+            self.y += 2
+
+    def get_cords(self):
+        return [self.x, self.y]
