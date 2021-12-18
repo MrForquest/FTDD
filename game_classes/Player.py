15fabe255a1b1fb1db1336f6ed2e7fefa5c7a668
Alex-sys-sys <94735806+Alex-sys-sys@users.noreply.github.com>
2021-12-18 12:29:42 +0300

    class player: мелкие фичи

--- game_classes/Player.py
+++ game_classes/Player.py
@@ -6,15 +6,16 @@ class Player(pp.sprite.Sprite):
         pp.sprite.Sprite.__init__(self)
         self.rect = pp.Rect(0, 0, 40, 40)
         self.screen = screen
+        self.hp = 100
 
     def update(self):
         pp.draw.rect(self.screen, (0, 0, 255), self.rect)
         key = pp.key.get_pressed()
-        if key[pp.K_LEFT]:
+        if key[pp.K_LEFT] and self.rect.x > 0:
             self.rect.x -= 2
-        if key[pp.K_RIGHT]:
+        if key[pp.K_RIGHT] and self.rect.x < 500:
             self.rect.x += 2
-        if key[pp.K_UP]:
+        if key[pp.K_UP] and self.rect.y > 0:
             self.rect.y -= 2
-        if key[pp.K_DOWN]:
+        if key[pp.K_DOWN] and self.rect.y < 500:
             self.rect.y += 2
